cdc5fb1bc8ad658f06ce60c04f598d39b07c1c18
andreyirk <sokolik_85@list.ru>
2018-12-04 05:19:27 -0500

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,15 @@
 package ru.job4j.calculate;
 
-public class Calculate
-{
-    public static void main(String[] args)
-    {
+/**
+ * Class
+ * @author  Andrey Sokolov
+ */
+public class Calculate {
+    /**
+     * Main
+     * @param args
+     */
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
