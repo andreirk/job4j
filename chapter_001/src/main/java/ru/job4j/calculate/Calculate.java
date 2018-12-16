48993291b09faf4373a66ddc3bf95480d0e51ad9
andreyirk <sokolik_85@list.ru>
2018-12-16 11:04:33 -0500

    1.5. Checkstyle[#91261]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -5,10 +5,7 @@ package ru.job4j.calculate;
  * @author  Andrey Sokolov
  */
 public class Calculate {
-    /**
-     * Main
-     * @param args
-     */
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
