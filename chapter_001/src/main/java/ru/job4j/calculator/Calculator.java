f95e20072a7138a0478a9e9b799a176ecab04ba7
andreyirk <sokolik_85@list.ru>
2018-12-17 04:25:50 -0500

    3.1. Элементарный калькулятор.[#91250]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    private double result;
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+    public void substact(double first, double second) {
+        this.result = first - second;
+    }
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+
+    public double getResult() {
+        return this.result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest  {
+    @Test
+    public void whenAddOnePlusOneThenTwo()  {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenSubstruct2From4Then2()  {
+        Calculator calc = new Calculator();
+        calc.substact(4D, 2D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenMultiply2By2Then4()  {
+        Calculator calc = new Calculator();
+        calc.multiple(2D, 2D);
+        double result = calc.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenDivide4by2Then2()  {
+        Calculator calc = new Calculator();
+        calc.div(4D, 2D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+}
