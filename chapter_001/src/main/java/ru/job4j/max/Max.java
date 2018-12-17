01ed9ae45e09a08f97055ca55d72199f0fb6b0aa
andreyirk <sokolik_85@list.ru>
2018-12-17 05:42:02 -0500

    4.2. Максимум из двух чисел

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,16 @@
+package ru.job4j.max;
+
+/**
+ * Корвертор валюты.
+ */
+public class Max {
+    /**
+     *
+     * @param first
+     * @param second
+     * @return int max value from two.
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
